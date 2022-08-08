a1f5936a2fa8e0ea320a5f766bd6fc4abb57b94f
Jack Mangosa <109428669+jackmangosa1@users.noreply.github.com>
2022-08-08 12:59:43 +0200

    Create navbar

---
+++ src/Stylesheet.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import './style.css'
+
+export default function Stylesheet(){
+    return(
+        <div>
+
+        </div>
+    )
+}
---
+++ src/conponents/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+export default function Navbar(){
+    return(
+        <nav className="navbar"> 
+            <img src="../images/logo.png" alt="logo" className="logo"/>
+        </nav>
+    )
+}
