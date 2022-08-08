23db05d0e7f988435d318a5d99d66a2c14378abe
Jack Mangosa <109428669+jackmangosa1@users.noreply.github.com>
2022-08-08 15:33:09 +0200

    Create Hero component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import Navbar from "./conponents/Navbar";
 import Stylesheet from "./Stylesheet";
+import Hero from "./conponents/Hero";
 
 export default function App(){
     return(
         <div>
             <Stylesheet/>
             <Navbar/>
+            <Hero/>
             
         </div>
     )
